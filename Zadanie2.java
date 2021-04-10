10b2ebeb34985f5eb74bae6966a9a924d6aba7a6
Michal Bujak <michalbujak7777@gmail.com>
2021-04-10 23:19:08 +0200

    #2 dodanie zadania nr 2 kody pocztowe

---
+++ Zadanie2.java
@@ -0,0 +1,26 @@
+public class Zadanie2 {
+    public static void main(String[] args) {
+        
+        int[] kod72603 = {7, 2, 6, 0, 3};
+        int[] kod72600 = {7, 2, 6, 0, 0};
+        int[] kod72602 = {7, 2, 6, 0, 2};
+
+        System.out.println("--------------");
+            System.out.println(
+                kod72603[0]+""+
+                kod72603[1]+""+
+                kod72603[2]+""+
+                kod72603[3]+"");
+                System.out.println(
+                kod72600[0]+""+
+                kod72600[1]+""+
+                kod72600[2]+""+
+                kod72600[3]+"");
+                System.out.println(
+                kod72602[0]+""+
+                kod72602[1]+""+
+                kod72602[2]+""+
+                kod72602[3]+"");
+        System.out.println("--------------");
+    }
+}
